b818fce4d0aa394c80e49b92aa14ffcbf2e2f83d
sachintech4 <sachintech4@gmail.com>
2023-04-23 17:27:36 +0530

    Update firebase-config.js

--- placement-management-ui/src/firebase-config.js
+++ placement-management-ui/src/firebase-config.js
@@ -1,27 +1,29 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const {
-    VITE_apiKey,
-    VITE_authDomain,
-    VITE_projectId,
-    VITE_storageBucket,
-    VITE_messagingSenderId,
-    VITE_appId,
-    VITE_measurementId,
+  VITE_apiKey,
+  VITE_authDomain,
+  VITE_projectId,
+  VITE_storageBucket,
+  VITE_messagingSenderId,
+  VITE_appId,
+  VITE_measurementId,
 } = import.meta.env;
 
 const firebaseConfig = {
-    apiKey: VITE_apiKey,
-    authDomain: VITE_authDomain,
-    projectId: VITE_projectId,
-    storageBucket: VITE_storageBucket,
-    messagingSenderId: VITE_messagingSenderId,
-    appId: VITE_appId,
-    measurementId: VITE_measurementId
-  };
+  apiKey: VITE_apiKey,
+  authDomain: VITE_authDomain,
+  projectId: VITE_projectId,
+  storageBucket: VITE_storageBucket,
+  messagingSenderId: VITE_messagingSenderId,
+  appId: VITE_appId,
+  measurementId: VITE_measurementId,
+};
 
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore();
+const app = initializeApp(firebaseConfig);
+const db = getFirestore();
+const auth = getAuth(app);
 
-  export { db };
+export { db, auth };
