d620fe1e4505db1e1190684e7a3c37352fe60f78
Sandeep Sharma <sandeeptech8@gmail.com>
2023-04-28 13:43:06 +0530

    Create Dashboard.jsx

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,48 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Grid } from "@adobe/react-spectrum";
+import { AuthUserContext } from "../contexts";
+import cons from "../cons";
+import Sidebar from "./Sidebar";
+import Content from "./Content";
+
+const users = Object.values(cons.USERS);
+
+function Dashboard() {
+  const authUser = useContext(AuthUserContext);
+  const [role, setRole] = useState(null);
+  const [selectedSidebarOption, setSidebarOption] = useState(null);
+
+  useEffect(() => {
+    const fetchUserDashboard = async () => {
+      if (authUser) {
+        const tokenResult = await authUser.getIdTokenResult();
+        const authRole = tokenResult.claims.role;
+        const _role = users.find((r) => r.type === authRole);
+        setRole(_role);
+      }
+    };
+    fetchUserDashboard();
+  }, [authUser]);
+  
+  return (
+    <Grid
+      height="100%"
+      areas={["sidebar content"]}
+      columns={["size-3600"]}
+      gap={"size-200"}
+    >
+      <Sidebar
+        gridArea="sidebar"
+        role={role}
+        onOptionSelect={setSidebarOption}
+      />
+      <Content
+        gridArea="content"
+        role={role}
+        selectedSidebarOption={selectedSidebarOption}
+      />
+    </Grid>
+  )
+}
+
+export default Dashboard;
