f9c91302838bb6889631f69d6a757704df3a56bc
Sandeep Sharma <sandeeptech8@gmail.com>
2023-04-28 13:43:17 +0530

    Create AddNewUser.jsx

---
+++ src/components/AddNewUser.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  Item,
+  TabList,
+  TabPanels,
+  Tabs,
+} from '@adobe/react-spectrum'
+import cons from "../cons";
+
+function AddTpo() {
+  return (
+    <View>
+      add new tpo form      
+    </View>
+  );
+}
+
+function AddStudent() {
+  return (
+    <View>
+      add new student user form
+    </View>
+  );
+}
+
+function AddNewUser() {
+  return (
+    <Tabs aria-label="add new users">
+      <TabList>
+        <Item key={cons.USERS.TPO.type}>{cons.USERS.TPO.text}</Item>
+        <Item key={cons.USERS.STUDENT.type}>{cons.USERS.STUDENT.text}</Item>
+      </TabList>
+      <TabPanels>
+        <Item key={cons.USERS.TPO.type}>
+          <AddTpo />
+        </Item>
+        <Item key={cons.USERS.STUDENT.type}>
+          <AddStudent />
+        </Item>
+      </TabPanels>
+    </Tabs>
+  );
+}
+
+export default AddNewUser;
