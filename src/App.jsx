f008321c9b8c49e9d5f5f155e659d64702a67ccf
Sandeep Sharma <sandeeptech8@gmail.com>
2023-04-28 13:40:56 +0530

    remove role sekector from the login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import React, { useState, useEffect } from "react";
 import { ToastContainer } from "@react-spectrum/toast";
-import { Button, lightTheme, Provider } from "@adobe/react-spectrum";
-
+import { lightTheme, Provider } from "@adobe/react-spectrum";
+import { onAuthStateChanged } from "@firebase/auth";
+import { auth } from "./firebase-config";
 import "./App.css";
+import Dashboard from "./components/Dashboard";
 import LoginScreen from "./components/LoginScreen";
+import { AuthUserContext } from "./contexts";
 
 function App() {
+  const [authUser, setAuthUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setAuthUser(user);
+      } else {
+        setAuthUser(null);
+      }
+    });
+
+    () => {
+      unsubscribe();
+    }
+  }, []);
+
   return (
     <Provider
       theme={lightTheme}
@@ -12,10 +32,14 @@ function App() {
       height={"100vh"}
       width={"100vw"}
     >
-      <LoginScreen />
-      <ToastContainer />
+      <AuthUserContext.Provider value={authUser}>
+        {authUser ? <Dashboard /> : <LoginScreen />}
+        <ToastContainer />
+      </AuthUserContext.Provider>
     </Provider>
   );
 }
 
 export default App;
+
+// todo: show loading screen on first load while firebase checks for auth in the background
