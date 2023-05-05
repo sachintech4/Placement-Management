8002a762bfa7d020d1c9a92730b766109a8a6f45
Sandeep Sharma <sandeeptech8@gmail.com>
2023-05-05 13:35:19 +0530

    setup separate contentRender functions for each role

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,18 +4,53 @@ import cons from "../cons";
 import AddNewUser from "./AddNewUser";
 import ProfileAndSettings from "./ProfileAndSettings";
 
-function Content({ gridArea, role, selectedSidebarOption }) {  
-  const renderContent = useCallback(() => {
-    switch(selectedSidebarOption?.type) {
+function Content({ gridArea, role, selectedSidebarOption }) {
+  const renderAdminContent = useCallback((sidebarOpt) => {
+    switch (sidebarOpt?.type) {
       case cons.SIDEBARS.COMMON.PROFILE_AND_SETTINGS.type: {
-        return <ProfileAndSettings role={role} />
+        return <ProfileAndSettings role={role} />;
       }
-      case cons.SIDEBARS.ADMIN.ADD_NEW_USER.type: {
-        return <AddNewUser />;
+      default:
+        return null;
+    }
+  }, []);
+
+  const renderTpoContent = useCallback((sidebarOpt) => {
+    switch (sidebarOpt?.type) {
+      case cons.SIDEBARS.COMMON.PROFILE_AND_SETTINGS.type: {
+        return <ProfileAndSettings role={role} />;
       }
-      default: return null;
+      default:
+        return null;
+    }
+  }, []);
+
+  const renderStudentContent = useCallback((sidebarOpt) => {
+    switch (sidebarOpt?.type) {
+      case cons.SIDEBARS.COMMON.PROFILE_AND_SETTINGS.type: {
+        return <ProfileAndSettings role={role} />;
+      }
+      default:
+        return null;
+    }
+  }, []);
+
+  const renderContent = useCallback(() => {
+    switch (role?.type) {
+      case cons.USERS.ADMIN.type: {
+        return renderAdminContent(selectedSidebarOption);
+      }
+      case cons.USERS.TPO.type: {
+        return renderTpoContent(selectedSidebarOption);
+      }
+      case cons.USERS.STUDENT.type: {
+        return renderStudentContent(selectedSidebarOption);
+      }
+      default:
+        return null;
     }
   }, [role, selectedSidebarOption]);
+
   return (
     <View
       gridArea={gridArea}
@@ -25,7 +60,7 @@ function Content({ gridArea, role, selectedSidebarOption }) {
     >
       {renderContent()}
     </View>
-  )
+  );
 }
 
 export default Content;
