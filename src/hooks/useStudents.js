1a75bd005fd3332f8a7230ea0b57b716cdc49c7c
Sandeep Sharma <sandeeptech8@gmail.com>
2023-05-05 16:59:16 +0530

    Create useStudents.js

---
+++ src/hooks/useStudents.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import { collection, onSnapshot } from "@firebase/firestore";
+import { db } from "../firebase-config";
+import cons from "../cons";
+
+function useStudents() {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    const userStudentsRef = collection(db, cons.DB.COLLECTIONS.USERS_STUDENT);
+
+    const unsubscribe = onSnapshot(userStudentsRef, (querySnapshot) => {
+      const data = querySnapshot.docs.map((doc) => doc.data());
+
+      if (data) setStudents(data);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return students;
+}
+
+export default useStudents;
