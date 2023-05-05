487312c30afb2e6d67b5885c833a558d723600e7
Sandeep Sharma <sandeeptech8@gmail.com>
2023-05-05 16:59:42 +0530

    setup table to render student list

--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -1,23 +1,73 @@
-import React, { useEffect } from "react";
-import { db } from "../firebase-config";
-import { collection, onSnapshot } from "@firebase/firestore";
-import cons from "../cons";
+import React, { useState, useEffect, useMemo } from "react";
+import {
+  TableView,
+  TableHeader,
+  Column,
+  TableBody,
+  Row,
+  Cell,
+  View,
+  SearchField,
+  Flex,
+} from "@adobe/react-spectrum";
+import useStudents from "../hooks/useStudents";
 
 function StudentList() {
+  const columns = useMemo(
+    () => [
+      { name: "Roll No", uid: "rollNo" },
+      { name: "Name", uid: "name" },
+      { name: "Email", uid: "email" },
+      { name: "Contact", uid: "contact" },
+      { name: "Placement status", uid: "placementStatus" },
+    ],
+    []
+  );
+  const [rows, setRows] = useState([]);
+  const students = useStudents();
+
   useEffect(() => {
-    const userStudentsRef = collection(db, cons.DB.COLLECTIONS.USERS_STUDENT);
-
-    const unsubscribe = onSnapshot(userStudentsRef, (querySnapshot) => {
-      const data = querySnapshot.docs.map((doc) => doc.data());
-      console.log("User students data:", data);
-    });
-
-    () => {
-      unsubscribe();
+    const prepareRows = () => {
+      const studentRows = students.map((sr, index) => ({
+        id: index,
+        rollNo: sr.rollNo,
+        name: `${sr.firstName} ${sr.lastName}`,
+        contact: sr.contactNumber ?? "Not available",
+        placementStatus: sr.isPlaced ? "Placed" : "Not placed",
+        email: sr.email,
+      }));
+      if (studentRows) setRows(studentRows);
     };
-  }, []);
+    prepareRows();
+  }, [students]);
 
-  return <div>student list</div>;
+  return (
+    <Flex height="100%" width="100%" direction={"column"} gap={"size-200"}>
+      <SearchField
+        label="Search"
+        onSubmit={(text) => {
+          console.log(text);
+        }}
+        width="size-3600"
+      />
+      <TableView aria-label="Students" width={"98%"} maxHeight={"98%"}>
+        <TableHeader columns={columns}>
+          {(column) => (
+            <Column
+              key={column.uid}
+              allowsResizing
+              allowsSorting={column.uid === "rollNo" || column.uid === "placementStatus"}
+            >
+              {column.name}
+            </Column>
+          )}
+        </TableHeader>
+        <TableBody items={rows}>
+          {(item) => <Row>{(columnKey) => <Cell>{item[columnKey]}</Cell>}</Row>}
+        </TableBody>
+      </TableView>
+    </Flex>
+  );
 }
 
 export default StudentList;
