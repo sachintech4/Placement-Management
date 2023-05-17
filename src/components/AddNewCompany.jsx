f473a073db63123c2a3c26b0686d531e17431c28
sachintech4 <sachintech4@gmail.com>
2023-05-17 18:44:43 +0530

    Adding a new company

---
+++ src/components/AddNewCompany.jsx
@@ -0,0 +1,121 @@
+import React, { useContext } from "react";
+import {
+  Form,
+  TextField,
+  Grid,
+  Button,
+  View,
+  Flex,
+  Heading,
+} from "@adobe/react-spectrum";
+import { ToastQueue } from "@react-spectrum/toast";
+import { AuthUserContext } from "../contexts";
+import cons from "../cons";
+
+function AddNewCompany() {
+  const user = useContext(AuthUserContext);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+
+    const companyName = formData.get("company-name").trim();
+    const location = formData.get("location").trim();
+    const email = formData.get("email").trim();
+
+    const details = {
+      companyName: companyName,
+      location: location,
+      email: email,
+    };
+
+    const data = {
+      details,
+      token: user.accessToken,
+    };
+
+    // validation
+    if (!details.companyName) {
+      ToastQueue.negative("Please provide the Company Name", { timeout: 1000 });
+      return;
+    }
+    if (!details.location) {
+      ToastQueue.negative("Please provide the Location", { timeout: 1000 });
+      return;
+    }
+    if (!details.email) {
+      ToastQueue.negative("Please provide the Email", { timeout: 1000 });
+      return;
+    }
+    if (!details.email.match(cons.REGEXS.VALID_EMAIL)) {
+      ToastQueue.negative("Please provide a valid Email", { timeout: 1000 });
+      return;
+    }
+
+    console.log(details);
+    console.log(data);
+
+    try {
+      const opts = {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      };
+
+      const res = await fetch(`${cons.BASE_SERVER_URL}/addNewCompany`, opts);
+      const resJson = await res.json();
+      if (resJson.code === "success") {
+        ToastQueue.positive(resJson.message, { timeout: 1000 });
+      } else if (resJson.code === "failed") {
+        ToastQueue.negative(resJson.message, { timeout: 1000 });
+      }
+    } catch (error) {
+      console.error("some error occured");
+    }
+  };
+
+  return (
+    <Flex direction="column" alignItems="start" gap={"size-200"}>
+      <View padding="size-200">
+        <Heading level={2}>Create new company</Heading>
+      </View>
+      <View
+        padding="size-250"
+        width="fit-content"
+        borderWidth="thin"
+        borderColor="dark"
+        borderRadius="medium"
+      >
+        <Form onSubmit={handleSubmit} isRequired>
+          <Grid
+            areas={["company-name location", "email email"]}
+            columns={"1fr 1fr"}
+            alignItems={"end"}
+            gap={"size-200"}
+          >
+            <TextField
+              label="Company Name"
+              gridArea={"company-name"}
+              name="company-name"
+            />
+            <TextField label="Location" gridArea={"location"} name="location" />
+            <TextField
+              label="Email"
+              gridArea={"email"}
+              width="100%"
+              name="email"
+            />
+            <View gridArea={"buttons"}>
+              <Button type="submit">Add</Button>
+            </View>
+          </Grid>
+        </Form>
+      </View>
+    </Flex>
+  );
+}
+
+export default AddNewCompany;
