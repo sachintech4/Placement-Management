5bb4ee60086d6657cead49ebfb180d6ab1cee2a8
sachintech4 <sachintech4@gmail.com>
2023-05-18 16:56:26 +0530

    ShowCompanies component to fetch and display the list of companies

---
+++ src/components/ShowCompanies.jsx
@@ -0,0 +1,164 @@
+import React, { useMemo, useState, useEffect } from "react";
+import {
+  TableView,
+  TableHeader,
+  Column,
+  TableBody,
+  Row,
+  Cell,
+  SearchField,
+  Flex,
+  useCollator,
+  Content,
+  Dialog,
+  DialogContainer,
+  Divider,
+  Heading,
+  Text,
+  Grid,
+  View,
+  Item,
+} from "@adobe/react-spectrum";
+import { ActionBar, ActionBarContainer } from "@react-spectrum/actionbar";
+import Delete from "@spectrum-icons/workflow/Delete";
+import { useAsyncList } from "react-stately";
+import useCompanies from "../hooks/useCompanies";
+import { debounce } from "../utils";
+
+function ShowCompanies() {
+  const companies = useCompanies();
+
+  const columns = useMemo(() => [
+    { name: "Company Name", uid: "companyName" },
+    { name: "Email", uid: "email" },
+    { name: "Location", uid: "location" },
+  ]);
+
+  const list = useAsyncList({
+    async load({ signal, filterText }) {
+      const prepareRows = () => {
+        const filteredAndNormalisedItems = [];
+        companies.forEach((cr) => {
+          const row = {
+            id: cr.uid,
+            companyName: cr.companyName,
+            email: cr.email,
+            location: cr.location,
+          };
+          const shouldRowBeInList = Object.values(row).some((cell) =>
+            cell?.toString().toLowerCase().includes(filterText.toLowerCase())
+          );
+          if (shouldRowBeInList) filteredAndNormalisedItems.push(row);
+        });
+        if (filteredAndNormalisedItems) return filteredAndNormalisedItems;
+        return [];
+      };
+
+      let items = prepareRows();
+      console.log(items);
+      return { items };
+    },
+  });
+
+  const [detailsDialog, setDetailsDialog] = useState(null);
+  const [selectedKeys, setSelectedKeys] = useState(new Set([]));
+
+  useEffect(() => {
+    list.reload();
+  }, [companies]);
+
+  const handleFilter = (text) => {
+    list.setFilterText(text.trim());
+  };
+  const handleSearchChange = debounce((text) => {
+    list.setFilterText(text.trim());
+  });
+  const handleRowAction = (key) => {
+    const row = list.items.find((item) => item.id === key);
+    const company = companies.find(
+      (company) => company.companyName === row.companyName
+    );
+    if (company) {
+      setDetailsDialog(company);
+    }
+  };
+
+  return (
+    <Flex height="100%" width="100%" direction={"column"} gap={"size-200"}>
+      <SearchField
+        label="Search"
+        onSubmit={handleFilter}
+        width="size-3600"
+        onChange={handleSearchChange}
+        onClear={() => list.setFilterText("")}
+      />
+      <ActionBarContainer height={"100%"} maxWidth={"100%"}>
+        <TableView
+          aria-label="Companies"
+          width={"98%"}
+          maxHeight={"98%"}
+          sortDescriptor={list.sortDescriptor}
+          onSortChange={list.sort}
+          onAction={handleRowAction}
+          selectionMode="multiple"
+          selectedKeys={selectedKeys}
+          onSelectionChange={setSelectedKeys}
+        >
+          <TableHeader columns={columns}>
+            {(column) => (
+              <Column
+                key={column.uid}
+                allowsResizing
+                // allowsSorting={column.uid === "rollNo"}
+              >
+                {column.name}
+              </Column>
+            )}
+          </TableHeader>
+          <TableBody items={list.items} loadingState={list.loadingState}>
+            {(item) => (
+              <Row>{(columnKey) => <Cell>{item[columnKey]}</Cell>}</Row>
+            )}
+          </TableBody>
+        </TableView>
+        <ActionBar
+          isEmphasized
+          selectedItemCount={selectedKeys === "all" ? "all" : selectedKeys.size}
+          // onAction={handleActionbarAction}
+          onClearSelection={() => setSelectedKeys(new Set())}
+        >
+          <Item key="delete">
+            <Delete />
+            <Text>Delete</Text>
+          </Item>
+        </ActionBar>
+      </ActionBarContainer>
+      <DialogContainer onDismiss={() => setDetailsDialog(null)}>
+        {detailsDialog && (
+          <Dialog isDismissable>
+            <Heading>{`${detailsDialog.companyName})`}</Heading>
+            <Divider />
+            <Content>
+              <Grid gap="size-200" columns={["1fr", "1fr"]}>
+                <View>
+                  <Heading level={4}>Email</Heading>
+                  <Text>{detailsDialog.email}</Text>
+                </View>
+                <View>
+                  <Heading level={4}>Location</Heading>
+                  <Text>{detailsDialog.location}</Text>
+                </View>
+                <View>
+                  <Heading level={4}>Placed Students</Heading>
+                  <Text>{detailsDialog.placedStudents}</Text>
+                </View>
+              </Grid>
+            </Content>
+          </Dialog>
+        )}
+      </DialogContainer>
+    </Flex>
+  );
+}
+
+export default ShowCompanies;
