76dd485a7e45c325940574c9aa48332cbbab44e0
sachintech4 <sachintech4@gmail.com>
2023-05-18 16:55:25 +0530

    setup a hook to fetch company data

---
+++ src/hooks/useCompanies.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import { collection, onSnapshot } from "@firebase/firestore";
+import { db } from "../firebase-config";
+import cons from "../cons";
+
+function useCompanies() {
+  const [companies, setCompanies] = useState([]);
+
+  useEffect(() => {
+    const userCompaniesRef = collection(db, cons.DB.COLLECTIONS.COMPANIES);
+
+    const unsubscribe = onSnapshot(userCompaniesRef, (querySnapshot) => {
+      const data = querySnapshot.docs.map((doc) => {
+        return { ...doc.data(), uid: doc.id };
+      });
+      if (data) setCompanies(data);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return companies;
+}
+
+export default useCompanies;
