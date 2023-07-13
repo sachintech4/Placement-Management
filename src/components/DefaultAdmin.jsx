337de540712d0874bfd23e24d9b90bb0ca18a4be
sachintech4 <sachintech4@gmail.com>
2023-07-13 19:13:30 +0530

    Default pages

--- src/components/DefaultAdmin.jsx
+++ src/components/DefaultAdmin.jsx
@@ -1,7 +1,30 @@
 import React, { useContext } from "react";
+import { View, Heading, Divider } from "@adobe/react-spectrum";
 
 function DefaultAdmin() {
-  return "Hello";
+  return (
+    <View
+      padding="size-250"
+      width="fit-content"
+      borderWidth="thin"
+      borderColor="dark"
+      borderRadius="medium"
+    >
+      <View paddingY="size-200">
+        <Heading level={1}>Hello, Admin!</Heading>
+      </View>
+      <Divider size="M" />
+      <View paddingY="size-200">
+        <Heading level={3}>Welcome to the Placement Management System.</Heading>
+        <Heading level={3}>
+          Naviagte through the options to add, delete or view users.
+        </Heading>
+        <Heading level={3}>
+          Look for Profile and Settings to upadate your Email ID or Password.
+        </Heading>
+      </View>
+    </View>
+  );
 }
 
 export default DefaultAdmin;
--- src/components/DefaultTpoPage.jsx
+++ src/components/DefaultTpoPage.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { View, Heading, Divider } from "@adobe/react-spectrum";
+import { db } from "../firebase-config";
+import { doc, onSnapshot } from "@firebase/firestore";
+import { AuthUserContext } from "../contexts";
+import cons from "../cons";
 
 function DefaultTpoPage() {
-  return "hello";
+  const user = useContext(AuthUserContext);
+  const [userName, setUserName] = useState(null);
+
+  useEffect(() => {
+    const getUserData = () => {
+      try {
+        const userDocRef = doc(db, cons.DB.COLLECTIONS.USERS_TPO, user.uid);
+
+        const unsubscribe = onSnapshot(userDocRef, (snapshot) => {
+          const documentData = snapshot.data();
+          if (documentData) {
+            setUserName(`${documentData.firstName} ${documentData.lastName}`);
+          }
+        });
+
+        return () => unsubscribe();
+      } catch (error) {
+        console.error("Error getting user data:", error);
+      }
+    };
+    getUserData();
+  }, []);
+
+  return (
+    <View
+      padding="size-250"
+      width="fit-content"
+      borderWidth="thin"
+      borderColor="dark"
+      borderRadius="medium"
+    >
+      <View paddingY="size-200">
+        <Heading level={1}>Hello, {userName}!</Heading>
+      </View>
+      <Divider size="M" />
+      <View paddingY="size-200">
+        <Heading level={3}>Welcome to the Placement Management System.</Heading>
+        <Heading level={3}>
+          Naviagte through the sidebar options according to your need.
+        </Heading>
+        <Heading level={3}>
+          Look for Profile and Settings to upadate your Email ID or Password.
+        </Heading>
+      </View>
+    </View>
+  );
 }
 
 export default DefaultTpoPage;
